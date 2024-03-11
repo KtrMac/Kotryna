e21812e1253cf2475f3f8bb144a1adce77f8a113
Kotryna <kotryna.macernyte@gmail.com>
2024-03-11 20:39:27 +0200

    ProjectK changeTxt

--- app/src/main/java/com/example/kotryna/MainActivity.java
+++ app/src/main/java/com/example/kotryna/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.kotryna;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvMain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMain);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
         });
+    }
+
+    public void btnChangeTxtClick(View view) {
+        this.tvMain.setText("Labas");
     }
 }
