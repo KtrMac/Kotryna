12fc29ec1b9f845e24e23a598fa252bc85d81ce6
Kotryna <kotryna.macernyte@gmail.com>
2024-03-11 21:01:23 +0200

    Revert "ProjectK add button 2"
    This reverts commit 2623740be9bced4e2d4622783bd7aaaa3a38fd35.

--- app/src/main/java/com/example/kotryna/MainActivity.java
+++ app/src/main/java/com/example/kotryna/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.kotryna;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,7 +13,6 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvMain;
-    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,22 +21,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.tvMain = findViewById(R.id.tvMain);
-        this.button2 = findViewById(R.id.button2);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
 
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Change the background color of Button2
-                button2.setBackgroundColor(getResources().getColor(R.color.button_pressed));
-            }
-        });
 
+        });
     }
 
     public void btnChangeTxtClick(View view) {
