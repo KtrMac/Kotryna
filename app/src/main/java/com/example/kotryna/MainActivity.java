5b1291d8fe5830ac6f63d619506f0763a5b5d0b2
Kotryna <kotryna.macernyte@gmail.com>
2024-03-26 22:11:22 +0200

    ProjectK add counter

--- app/src/main/java/com/example/kotryna/MainActivity.java
+++ app/src/main/java/com/example/kotryna/MainActivity.java
@@ -27,8 +27,6 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        this.tvMain = findViewById(R.id.tvMain);
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
